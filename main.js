5cc468a2060dcd040d3f5408ce13c3c64edd00c7
Secret4907 <113611720+Secret4907@users.noreply.github.com>
2023-08-01 09:55:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var sketch = 0
+var randomNumber = Math.floor(Math.random()*quickDrawDataset)
+console.log(randomNumber)
+document.getElementById("esbocopedido").innerHTML = sketch
+var timerCounter = 0
+var timerCheck = []
+var drawSketch = []
+var answerHolder = []
+var score = 0
